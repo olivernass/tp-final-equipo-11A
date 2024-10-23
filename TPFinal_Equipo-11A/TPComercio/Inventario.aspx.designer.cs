3d508e29cd26332b0bb60e71f200cdeaff98e7ec
rodriguezleandro98 <63465148+rodriguezleandro98@users.noreply.github.com>
2024-10-22 21:28:45 -0300

    pag producto con activar y desactivar

--- TPFinal_Equipo-11A/TPComercio/Inventario.aspx.designer.cs
+++ TPFinal_Equipo-11A/TPComercio/Inventario.aspx.designer.cs
@@ -11,7 +11,16 @@ namespace TPComercio
 {
 
 
-    public partial class Default
+    public partial class Inventario
     {
+
+        /// <summary>
+        /// Control rptProductos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptProductos;
     }
 }
