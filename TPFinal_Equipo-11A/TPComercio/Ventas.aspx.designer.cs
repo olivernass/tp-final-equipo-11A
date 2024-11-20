3feb712deee7dba276c1b6aba54d5b6c1a7dad73
AntonioMelino <antoniomelino1997@gmail.com>
2024-11-19 23:03:55 -0300

    Ventas - seleccion de cliente desde un filtro en una tabla

--- TPFinal_Equipo-11A/TPComercio/Ventas.aspx.designer.cs
+++ TPFinal_Equipo-11A/TPComercio/Ventas.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace TPComercio
 
     public partial class Ventas
     {
+
+        /// <summary>
+        /// Control txtFiltroDNIClientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFiltroDNIClientes;
+
+        /// <summary>
+        /// Control btnBorrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBorrar;
+
+        /// <summary>
+        /// Control rptVentasClientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptVentasClientes;
+
+        /// <summary>
+        /// Control btnVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVolver;
     }
 }
